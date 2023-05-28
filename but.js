398d6fc08a50ae4c92519640188f512ac1b341f3
Akshaja V Maiya <82953729+Akshajaease123@users.noreply.github.com>
2023-05-28 06:13:45 +0530

    Add files via upload

---
+++ but.js
@@ -0,0 +1,17 @@
+function but1(){
+    window.location.href = "faculty.html";
+}
+ 
+function but2(){
+    window.location.href = "stdreg.html";
+}
+function but3(){
+    window.location.href = "dep.html";
+}
+
+function checkform(){
+    if(stdreg.html.form==""){
+        alert("Please complete form");
+        return false;
+    }
+    }
